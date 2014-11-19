d8be4b90f9217fa244d2fec71cd64dc294624dd4
Alexandre Terrasa <alexandre@moz-code.org>
2014-11-18 19:24:18 -0500

    Add GET /news/:city service
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var data = require('../data/cities');
 
@@ -12,6 +13,34 @@ router.get('/cities/', function(req, res) {
 	res.json(cityNames());
 });
 
+/* GET news article from file */
+router.get('/news/:city', function(req, res) {
+	var city = req.params.city;
+	if(!checkCity(city)) {
+		respondCityError(city, res);
+	}
+	var file = data.cities[city].file;
+	fs.readFile('data/' + file, function(err, data) {
+		if(err) {
+			res.status(500);
+			res.render("public_error", {
+				message: "Internal server error, try later.",
+				reason: "No news to display for city " + city + "."
+			});
+			return;
+		}
+		res.json({
+			title: "À la une",
+			content: data.toString()
+		});
+	});
+});
+
+/* Check if the city is in the `cities` list. */
+function checkCity(city) {
+	return data.cities[city]
+}
+
 /* Get a list of the city names */
 function cityNames() {
 	var cities = [];
@@ -19,6 +48,15 @@ function cityNames() {
 		cities.push(i);
 	}
 	return cities;
+}
+
+/* Respond with error for a unknown city. */
+function respondCityError(city, res) {
+	res.status(400);
+	res.render("public_error", {
+		message: "Bad Request: Invalid city `" + city + "`.",
+		reason: "Should be one of " + cityNames()
+	});
 }
 
 module.exports = router;
