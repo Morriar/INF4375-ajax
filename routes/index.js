896f9ad6e6016664f88442c7c0075b7ac648a672
Alexandre Terrasa <alexandre@moz-code.org>
2014-11-18 19:23:09 -0500

    Add GET /cities service
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/cities');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET cities list */
+router.get('/cities/', function(req, res) {
+	res.json(cityNames());
+});
+
+/* Get a list of the city names */
+function cityNames() {
+	var cities = [];
+	for(i in data.cities) {
+		cities.push(i);
+	}
+	return cities;
+}
 
 module.exports = router;
