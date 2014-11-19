eaa4c58eb50137137899397141476571cff6e95d
Alexandre Terrasa <alexandre@moz-code.org>
2014-11-18 19:31:35 -0500

    Add correction
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,7 +6,22 @@ function getSelectedCity() {
 
 // Load city list from REST service
 function loadCities() {
-	// TODO
+	var request = new XMLHttpRequest();
+	var url = "/cities";
+
+	request.open("GET", url, true);
+	request.onreadystatechange = function() {
+		if (request.readyState === 4 && request.status === 200) {
+			var select = document.getElementById("city_selector");
+			var cities = JSON.parse(request.responseText);
+			for(i in cities) {
+				var opt = document.createElement("option");
+				opt.innerHTML = cities[i]
+				select.appendChild(opt);
+			}
+		}
+	};
+	request.send();
 }
 
 // Load city portal panel
@@ -18,12 +33,38 @@ function loadCity() {
 
 // Load weather div
 function loadWeather(city) {
-	// TODO
+	var request = new XMLHttpRequest();
+	var url = "/weather/" + city;
+
+	request.open("GET", url, true);
+	request.onreadystatechange = function() {
+		if (request.readyState === 4 && request.status === 200) {
+			var xml = request.responseText;
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(xml, "text/xml");
+			var temp = doc.getElementsByTagName('temp')[0];
+			var div = document.getElementById("weather");
+			console.log(div);
+			div.innerHTML = "Il fait " + temp.innerHTML + " à " + city;
+		}
+	};
+	request.send();
 }
 
 // Load news div
 function loadNews(city) {
-	// TODO
+	var request = new XMLHttpRequest();
+	var url = "/news/" + city;
+
+	request.open("GET", url, true);
+	request.onreadystatechange = function() {
+		if (request.readyState === 4 && request.status === 200) {
+			var json = JSON.parse(request.responseText);
+			var div = document.getElementById("news");
+			div.innerHTML = json.content;
+		}
+	};
+	request.send();
 }
 
 // Load city admin panel
@@ -34,5 +75,23 @@ function loadAdmin() {
 
 // Push news info
 function submitNews() {
-	// TODO
+	var city = getSelectedCity();
+	//Data to push
+	var jsonData = JSON.stringify({
+		city: city,
+		content: document.getElementById("news").value
+	});
+
+	var request = new XMLHttpRequest();
+	var url = "/news/" + city;
+
+	request.open("PUT", url, true);
+	request.setRequestHeader("Content-type", "application/json");
+	request.onreadystatechange = function() {
+		if (request.readyState === 4 && request.status === 200) {
+			var div = document.getElementById("messages");
+			div.innerHTML = request.responseText;
+		}
+	};
+	request.send(jsonData);
 }
